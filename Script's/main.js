3f2621ca999e7206240a2226d6e2005d9c6e4227
RaphaelFiore <raphael.fiore.tec@gmail.com>
2024-10-08 01:33:12 -0700

    Atualizacao

--- Script's/main.js
+++ Script's/main.js
@@ -1,10 +1,34 @@
-const botao = document.getElementById('Btn2');
-const sidebar = document.getElementById('sidebar');
+const biografia = document.getElementById('biografia2');
+const services = document.getElementById('services');
+const imgLinus = document.querySelector('.imgLinus')
 
-botao.addEventListener('click', function() {
-    if (sidebar.style.width == '6vw') {
-        sidebar.style.width = '0';
-    } else {
-        sidebar.style.width = '6vw';
+document.getElementById('botao-1').addEventListener('click', function() {
+    services.style.display = 'none';
+
+    biografia.style.display = 'block';
+    window.revelar.reveal('.efeito-box1', {
+        duration: 1400,
+        distance: '90px'
+    });
+    if (imgLinus){
+        imgLinus.remove();
     }
-});;
+});
+
+
+// const biografia = document.getElementById('biografia2');
+// const botao = document.getElementById('botao-1');
+
+// addEventListener('click', function(){
+//     biografia.scrollIntoView({
+//         behavior: 'smooth'
+//     });
+// });;
+
+// botao.addEventListener('click', function() {
+//     if (sidebar.style.width == '6vw') {
+//         sidebar.style.width = '0';
+//     } else {
+//         sidebar.style.width = '6vw';
+//     }
+// });;
